a635cea1c3225833c92884a13d09d7ea9a326400
Jaseelapa <145674999+Jaseelapa@users.noreply.github.com>
2024-07-05 11:29:09 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+package Figures;
+public class Rectangle
+{
+private double length;
+private double breadth;
+public Rectangle(double length, double breadth)
+{
+this.length = length;
+this.breadth = breadth;
+}
+public double calcArea()
+{
+double ar=length * breadth;
+return ar;
+}
+}
+
