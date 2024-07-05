ff9fffbe2af52e44f973fd7f0a28097f5c1e0d36
Jaseelapa <145674999+Jaseelapa@users.noreply.github.com>
2024-07-05 11:30:16 +0530

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,15 @@
+package arithmetic;
+public class Substraction
+{
+private double num1;
+private double num2;
+public Division(double num1, double num2)
+{
+this.num1 =num1;
+this.num2= num2;
+}
+public double sub()
+{
+return num1 / num2;
+}
+} 
